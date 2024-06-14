84b87f6e54d2cc2a41b91155e2233ae9faddb621
carlospasichana <pasichanacarlos3@gmail.com>
2024-06-14 17:35:51 -0500

    finalizacion paginas

---
+++ scripts/productos.js
@@ -0,0 +1,60 @@
+$('#btn_alerta').click(function(){
+    Swal.fire("Articulo agregado al carrito de compras!");
+
+});
+
+$('#btn_img2').click(function(){
+    Swal.fire("Articulo agregado al carrito de compras!");
+
+});
+
+$('#btn_img3').click(function(){
+    Swal.fire("Articulo agregado al carrito de compras!");
+
+});
+
+$('#btn_img4').click(function(){
+    Swal.fire("Articulo agregado al carrito de compras!");
+
+});
+
+$('#btn_img5').click(function(){
+    Swal.fire("Articulo agregado al carrito de compras!");
+
+});
+
+$('#btn_img6').click(function(){
+    Swal.fire("Articulo agregado al carrito de compras!");
+
+});
+
+$('#btn_img7').click(function(){
+    Swal.fire("Articulo agregado al carrito de compras!");
+
+});
+
+$('#btn_img8').click(function(){
+    Swal.fire("Articulo agregado al carrito de compras!");
+
+});
+
+$('#btn_img9').click(function(){
+    Swal.fire("Articulo agregado al carrito de compras!");
+
+});
+
+$('#btn_img10').click(function(){
+    Swal.fire("Articulo agregado al carrito de compras!");
+
+});
+
+$('#btn_img11').click(function(){
+    Swal.fire("Articulo agregado al carrito de compras!");
+
+});
+
+$('#btn_img12').click(function(){
+    Swal.fire("Articulo agregado al carrito de compras!");
+
+});
+
